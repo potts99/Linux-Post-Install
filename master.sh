d17834f07c2b47c75ea1075d56706607d96c7ddc
Jack Andrews <45759583+jwandrews99@users.noreply.github.com>
2020-02-10 18:43:03 +0000

    master set up

---
+++ master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Goal: Script which automatically sets up a new Ubuntu Machine after installation
+# This is a basic install, easily configurable to your needs
+
+# Test to see if user is running with root privileges.
+if [[ "${UID}" -ne 0 ]]
+then
+ echo 'Must execute with sudo or root' >&2
+ exit 1
+fi
+
