816c2a7e869dcbc40bbaca5b97f34fc244321ef3
Jack Andrews <45759583+jwandrews99@users.noreply.github.com>
2020-02-14 15:07:00 +0000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -83,7 +83,7 @@ Do you want to install docker? If so type y / If you dont want to install enter 
 "
 read $docker
 
-if [[ $docker -eq "y" ]] || [[ $docker -eq "yes" ]]; then
+if [[ $docker -eq "y" ]] || [[ $docker -eq "yes" ]] ; then
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
